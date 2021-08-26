39805d124ef0a24050520533ca4c9f8a308c5cb4
fictionalparakeets <banksmatthew32@gmail.com>
2021-08-26 19:17:40 -0400

    update database seed files, db reset in working state

--- src/application.js
+++ src/application.js
@@ -10,9 +10,9 @@ const app = express();
 
 const db = require("./db");
 
-const days = require("./routes/days");
-const appointments = require("./routes/appointments");
-const interviewers = require("./routes/interviewers");
+// const days = require("./routes/days");
+// const appointments = require("./routes/appointments");
+// const interviewers = require("./routes/interviewers");
 
 function read(file) {
   return new Promise((resolve, reject) => {
@@ -37,9 +37,9 @@ module.exports = function application(
   app.use(helmet());
   app.use(bodyparser.json());
 
-  app.use("/api", days(db));
-  app.use("/api", appointments(db, actions.updateAppointment));
-  app.use("/api", interviewers(db));
+  // app.use("/api", days(db));
+  // app.use("/api", appointments(db, actions.updateAppointment));
+  // app.use("/api", interviewers(db));
 
   if (ENV === "development" || ENV === "test") {
     Promise.all([
@@ -49,7 +49,8 @@ module.exports = function application(
       .then(([create, seed]) => {
         app.get("/api/debug/reset", (request, response) => {
           db.query(create)
-            .then(() => db.query(seed))
+            .then(() => {
+              db.query(seed)})
             .then(() => {
               console.log("Database Reset");
               response.status(200).send("Database Reset");
--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -1,35 +1,86 @@
-DROP TABLE IF EXISTS available_interviewers CASCADE;
-DROP TABLE IF EXISTS interviews CASCADE;
-DROP TABLE IF EXISTS interviewers CASCADE;
-DROP TABLE IF EXISTS appointments CASCADE;
-DROP TABLE IF EXISTS days CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users_events CASCADE;
+DROP TABLE IF EXISTS events CASCADE;
+DROP TABLE IF EXISTS kanban_board CASCADE;
+DROP TABLE IF EXISTS swimlane CASCADE;
+DROP TABLE IF EXISTS do_item CASCADE;
+DROP TABLE IF EXISTS vendors_events CASCADE;
+DROP TABLE IF EXISTS vendors CASCADE;
 
-CREATE TABLE days (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  planner_role BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE appointments (
+
+CREATE TABLE events (
   id SERIAL PRIMARY KEY NOT NULL,
-  time VARCHAR(255) NOT NULL,
-  day_id INTEGER REFERENCES days(id) ON DELETE CASCADE
+  title TEXT,
+  first_name VARCHAR(255) NOT NULL,
+  second_name VARCHAR(255) NOT NULL,
+  event_date_weekday TEXT,
+  event_date_day INTEGER,
+  event_date_month TEXT,
+  event_date_year INTEGER,
+  email VARCHAR(225) NOT NULL,
+  phone_number VARCHAR(20),
+  unit VARCHAR(10),
+  street_number VARCHAR(10) NOT NULL,
+  street_name VARCHAR(255) NOT NULL,
+  street_type TEXT NOT NULL,
+  postal_code VARCHAR(7) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  date_created TIMESTAMP DEFAULT now(),
+  percentage INTEGER
 );
 
-CREATE TABLE interviewers (
+CREATE TABLE users_events (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  avatar VARCHAR(255) NOT NULL
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  event_id INTEGER REFERENCES events(id) ON DELETE CASCADE
 );
 
-CREATE TABLE interviews (
+CREATE TABLE kanban_board (
   id SERIAL PRIMARY KEY NOT NULL,
-  student VARCHAR(255) NOT NULL,
-  interviewer_id INTEGER REFERENCES interviewers(id) ON DELETE CASCADE,
-  appointment_id INTEGER UNIQUE REFERENCES appointments(id) ON DELETE CASCADE
+  event_id INTEGER REFERENCES events(id) ON DELETE CASCADE,
+  name varchar(50) NOT NULL
 );
 
-CREATE TABLE available_interviewers (
+CREATE TABLE swimlane (
   id SERIAL PRIMARY KEY NOT NULL,
-  day_id INTEGER REFERENCES days(id) ON DELETE CASCADE,
-  interviewer_id INTEGER REFERENCES interviewers(id) ON DELETE CASCADE
+  board_id INTEGER REFERENCES kanban_board(id) ON DELETE CASCADE,
+  status TEXT NOT NULL,
+  name varchar(50) NOT NULL
 );
+
+CREATE TABLE do_item (
+  id SERIAL PRIMARY KEY NOT NULL,
+  swimlane_id INTEGER REFERENCES swimlane(id) ON DELETE CASCADE,
+  date_added TIMESTAMP DEFAULT now(),
+  status TEXT NOT NULL,
+  title TEXT NOT NULL,
+  description TEXT,
+  date_due TIMESTAMP
+);
+
+-- CREATE TABLE vendors_events (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   vendor_id INTEGER REFERENCES vendors(id) ON DELETE CASCADE,
+--   event_id INTEGER REFERENCES events(id) ON DELETE CASCADE,
+--   vendor_type TEXT
+-- );
+
+-- CREATE TABLE vendors (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   business_name VARCHAR(255) NOT NULL,
+--   contact_name VARCHAR(255) NOT NULL,
+--   email VARCHAR(225) NOT NULL,
+--   phone_number VARCHAR(20),
+--   street_number VARCHAR(10) NOT NULL,
+--   street_name VARCHAR(255) NOT NULL,
+--   city VARCHAR(255) NOT NULL,
+--   postal_code VARCHAR(7) NOT NULL,
+--   website_url VARCHAR(255)
+-- );
