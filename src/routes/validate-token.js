d2eb06c9fc83183980e6c19de971f9a4a20859c9
S Avery <sarahashavery@gmail.com>
2021-08-29 19:03:41 -0400

    Update to authorization. Routes are protected - Only visable to user

--- src/routes/validate-token.js
+++ src/routes/validate-token.js
@@ -2,16 +2,26 @@ const jwt = require("jsonwebtoken");
 
 // middleware to validate token
 const verifyToken = (req, res, next) => {
-  const token = req.header("auth-token");
-  if (!token) return res.status(401).json({ error: "Access denied" });
+  const bearerHeader = req.headers["authorization"]; // 'Authorization: Bearer eyJraWQiOi...'
+
+  if (!bearerHeader) {
+    // Forbidden
+    res.status(401).json({ error: "Access denied" });
+  }
+
+  const bearer = bearerHeader.split(" "); // 'Bearer eyJraWQiOi...'
+  const bearerToken = bearer[1];
 
   try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    const verified = jwt.verify(bearerToken, process.env.ACCESS_TOKEN_SECRET);
     req.user = verified;
-    console.log('req.user: ', req.user)
+    console.log("req.user: ", req.user);
     next();
   } catch (err) {
-    res.status(400).json({ error: "Token is not valid" });
+    res.status(401).json({ error: "Token is not valid" });
+
+    // if we had resource permissions, would check here
+    // 200 or 403 unauthorized
   }
 };
 
