92385e98c7e1f8e9fb93ff6c396e308088b63b3e
fictionalparakeets <banksmatthew32@gmail.com>
2021-08-31 21:13:07 -0400

    update insert query for new events

--- src/routes/users_events.js
+++ src/routes/users_events.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-
   // Get all events for a user
   router.get("/", (req, res) => {
     if (!req.user.id) res.status(404).json({ error: "User id not provided" });
@@ -41,19 +40,22 @@ module.exports = (db) => {
       });
   });
 
-
   // Add a new event for this user - Will be called by submission of 'add new event' form
   router.put("/:userId/add", (req, res) => {
     if (!req.user.id) res.status(404).json({ error: "User id not provided" });
 
     // Double check values and order from client-side
-    const values = Object.values(req.body)
+    const values = Object.values(req.body);
+    // Check the values numbers. user.id will also be part of the values array
 
     db.query(
       `
-      INSERT INTO events (title, first_name, second_name, event_date, email, phone, unit, street_number, street_name, street_type, postal_code, city)
-      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-      RETURNING *;
+      WITH event_key AS
+      (INSERT INTO events (title, first_name, second_name, event_date, email, phone, unit, street_number, street_name, street_type, postal_code, city)
+      VALUES
+      ('TEST!!!!', 'Frank Alistair', 'Georgia Green', '2016-06-22 22:10:25-04', 'fngwedding@email.com', '4168261456', '23A', 145, 'Brooklands', 'Place', 'M2X 4W9', 'Cityville') RETURNING id)
+      INSERT INTO users_events (user_id, event_id)
+      VALUES (1, (SELECT id FROM event_key));
         `,
       values
     )
@@ -67,8 +69,6 @@ module.exports = (db) => {
       });
   });
 
-
-  
   // Delete an event for this user - Will be called by delete option on ellipsis menu of each event displayed on Dashboard
   router.delete("/:eventId/delete", (req, res) => {
     db.query(
@@ -86,9 +86,6 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
-
-
-
 
   return router;
 };
