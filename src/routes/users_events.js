bc03273e91428758c745697c3fb669d0e1a4b184
fictionalparakeets <banksmatthew32@gmail.com>
2021-09-01 13:31:24 -0400

    update query and post route for new events

--- src/routes/users_events.js
+++ src/routes/users_events.js
@@ -41,32 +41,34 @@ module.exports = (db) => {
   });
 
   // Add a new event for this user - Will be called by submission of 'add new event' form
-  router.put("/:userId/add", (req, res) => {
-    if (!req.user.id) res.status(404).json({ error: "User id not provided" });
-
-    // Double check values and order from client-side
+  router.post("/add", (req, res) => {
     const values = Object.values(req.body);
-    // Check the values numbers. user.id will also be part of the values array
-
+    // $13 is the userId
     db.query(
       `
       WITH event_key AS
-      (INSERT INTO events (title, first_name, second_name, event_date, email, phone, unit, street_number, street_name, street_type, postal_code, city)
+      (INSERT INTO events 
+        (title,
+        first_name,
+        second_name,
+        event_date,
+        email,
+        phone,
+        unit,
+        street_number,
+        street_name,
+        street_type,
+        postal_code,
+        city)
       VALUES
-      ('TEST!!!!', 'Frank Alistair', 'Georgia Green', '2016-06-22 22:10:25-04', 'fngwedding@email.com', '4168261456', '23A', 145, 'Brooklands', 'Place', 'M2X 4W9', 'Cityville') RETURNING id)
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING id)
       INSERT INTO users_events (user_id, event_id)
-      VALUES (1, (SELECT id FROM event_key));
+      VALUES ($13, (SELECT id FROM event_key));
         `,
       values
     )
-      .then((data) => {
-        // const events = data.rows;
-        // console.log(events);
-        // res.json(events);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+      .then((data) => res.sendStatus(200))
+      .catch((err) => res.status(500).json({ error: err.message }));
   });
 
   // Delete an event for this user - Will be called by delete option on ellipsis menu of each event displayed on Dashboard
